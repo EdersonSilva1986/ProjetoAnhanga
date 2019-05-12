9fd361429735f50c6585966071daf950514a692a
Ederson Silva <esilva@meeta.com.br>
2019-05-12 15:52:13 -0300

    comentario com as alteraþ§es feitas

--- Anhanga.api/bin/configuration/variables.js
+++ Anhanga.api/bin/configuration/variables.js
@@ -1,6 +1,13 @@
 const variables = {
     Api: {
         port: process.env.port || 3000
+    },
+    Database: {
+        //conexão local com o mongodb instalado no pc
+        connection: process.env.connection || 'mongodb://127.0.0.1:27017/AnhangaFood?gssapiServiceName=mongodb' 
+        //conexão com o mongodb na nuvem através do site MongoDB.Atlas
+        //connection: process.env.connection || 'mongodb+srv://mnihouze_usr_app:app753951@cluster0-g9gen.mongodb.net/AnhangaFood?retryWrites=true'
     }
 }
 module.exports = variables;
+
